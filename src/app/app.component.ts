e871a23057b0a42a62fb91f440bc0a50249e63b5
Rahul Navale <rsaadmin@c3itspdemo.com>
2020-03-17 06:07:30 +0000

    NavBar Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,30 +4,23 @@ import { Component } from "@angular/core";
   selector: 'pm-root',
   styleUrls: ['./app.component.css'],
   template: `
-  <div class="toolbar" role="banner">
+  <nav class="navbar navbar-expand-lg navbar-light bg-dark">
   <img
     width="40"
     alt="Angular Logo"
     src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg=="
   />
-  <span>
-  <ul class='nav nav-pills'>
-  <li><a class='nav-link' [routerLink]="['/welcome']" style="color: white;" >Home</a></li>
-  <li><a class='nav-link' [routerLink]="['/products']" style="color: white;" > Product List</a></li>
-  <li><a class='nav-link' [routerLink]="['/new']" style="color: white;" > New Product </a></li>
-
-</ul>
-
-</span>
-    <div class="spacer"></div>
-    <a aria-label="Angular on twitter" target="_blank" rel="noopener" href="https://twitter.com/iamrahulsn" title="Twitter">
-      <svg id="twitter-logo" height="24" data-name="Logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
-        <rect width="400" height="400" fill="none"/>
-        <path d="M153.62,301.59c94.34,0,145.94-78.16,145.94-145.94,0-2.22,0-4.43-.15-6.63A104.36,104.36,0,0,0,325,122.47a102.38,102.38,0,0,1-29.46,8.07,51.47,51.47,0,0,0,22.55-28.37,102.79,102.79,0,0,1-32.57,12.45,51.34,51.34,0,0,0-87.41,46.78A145.62,145.62,0,0,1,92.4,107.81a51.33,51.33,0,0,0,15.88,68.47A50.91,50.91,0,0,1,85,169.86c0,.21,0,.43,0,.65a51.31,51.31,0,0,0,41.15,50.28,51.21,51.21,0,0,1-23.16.88,51.35,51.35,0,0,0,47.92,35.62,102.92,102.92,0,0,1-63.7,22A104.41,104.41,0,0,1,75,278.55a145.21,145.21,0,0,0,78.62,23" fill="#fff"/>
-      </svg>
-    </a>
-</div>
-<div class="spacer"></div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+    <li><a class='nav-link' [routerLink]="['/welcome']" style="color: white;" >Home</a></li>
+    <li><a class='nav-link' [routerLink]="['/products']" style="color: white;" > Product List</a></li>
+    <li><a class='nav-link' [routerLink]="['/new']" style="color: white;" > New Product </a></li>
+    </div>
+  </div>
+</nav>
              <router-outlet></router-outlet>
 
   `
